d3a250c8d33d675941aac14f4fcaf1195c4bab2a
xuyujie <xuyujie.brbi@bytedance.com>
2026-08-28 11:24:07 +0800

    Disable dev locator in production

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   build: {
     sourcemap: 'hidden',
   },
@@ -14,11 +14,9 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [
-          'react-dev-locator',
-        ],
+        plugins: mode === 'development' ? ['react-dev-locator'] : [],
       },
     }),
     tsconfigPaths()
   ],
-})
+}))
